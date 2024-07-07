728c36305850720c0e9e1fa23706c5ae684b55d6
alvaro-ruizcalzada-uah <alvaro-ruizcalzada@edu.uah.es>
2024-07-07 20:56:03 +0200

    first commit

---
+++ src/main/java/es/uah/filmAlvaritty/FilmAlvarittyApplication.java
@@ -0,0 +1,13 @@
+package es.uah.filmAlvaritty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmAlvarittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmAlvarittyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/filmAlvaritty/FilmAlvarittyApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.filmAlvaritty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAlvarittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
